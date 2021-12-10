7acc3b9880b1598010557dae53f1939cba461270
Anna <a.trofimova@altcentr.ru>
2021-12-11 02:05:44 +0500

    Course 2 homework 6 to check

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         List<String> quote = new ArrayList<>(List.of("easy", "come", "easy", "go"));
