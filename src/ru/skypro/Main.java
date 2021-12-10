ab0a9826049f04eba87dd7cee83d63ef6d0540ef
Anna <a.trofimova@altcentr.ru>
2021-12-11 02:05:03 +0500

    Course 2 homework 6 to check

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        List <String> quote = new ArrayList<>(List. of ("easy", "come", "easy", "go"));
+        List<String> quote = new ArrayList<>(List.of("easy", "come", "easy", "go"));
         System.out.println("Odd numbers: " + getOddNums(nums));
         System.out.println("Even numbers without dublicates: " + getUniqueEvenNums(nums));
         System.out.println("Changed quote without duplicated words: " + getUniqueWords(quote));
@@ -37,22 +37,22 @@ public class Main {
         return evenNums;
     }
 
-    public static List getUniqueWords (List <String> quote) {
+    public static List getUniqueWords(List<String> quote) {
         Set<String> set = new LinkedHashSet<>(quote);
         quote.clear();
         quote.addAll(set);
         List<String> newQuote = new ArrayList<>();
         for (int i = 0; i < quote.size(); i++) {
-                newQuote.add(quote.get(i));
+            newQuote.add(quote.get(i));
         }
         return newQuote;
     }
 
-    public static int countDuplicates (List <String> quote) {
-        int count=0;
-        Iterator <String> iterator = quote.iterator();
+    public static int countDuplicates(List<String> quote) {
+        int count = 0;
+        Iterator<String> iterator = quote.iterator();
         while (iterator.hasNext()) {
-            String next = iterator.next ();
+            String next = iterator.next();
             if (quote.get(0).equals(next)) {
                 count++;
             }
